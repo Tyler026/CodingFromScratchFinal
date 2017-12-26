870331a2a829088c08a0bd97dc897af1adadfe6a
Tyler Fuqua <tyler@agilityda.com>
2017-12-26 13:53:25 -0700

    CSS

--- script.js
+++ script.js
@@ -10,18 +10,18 @@ var phoneNumber = document.getElementById("phoneNumber").value;
 // Post Info 
 document.getElementById("postFullName").innerHTML = firstName + " " + lastName;
 document.getElementById("postAddress").innerHTML = address;
-document.getElementById("postPhoneNumber").innerHTML = phoneNumber;
-// Empty Array
-var  numberArray = []
-numberArray.push (age, phoneNumber)
-console.log(numberArray);
-
-  for (var i = age, numberArray; i < numberArray; i++) {
-        if ( i <= 100) {
-            document.getElementById("postAge").innerHTML = "Age: " + age;
-        } else {
-            document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
-        }
-    } 
-
+document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+document.getElementById("postAge").innerHTML = "Age: " + age;
 }
+// Empty Array
+// var  numberArray = []
+// numberArray.push (age, phoneNumber)
+// console.log(numberArray);
+
+//  for (var i = age, numberArray; i < numberArray; i++) {
+//         if ( i <= 100) {
+//             document.getElementById("postAge").innerHTML = "Age: " + age;
+//         } else {
+//             document.getElementById("postPhoneNumber").innerHTML = "Phone Number: " + phoneNumber;
+//         }
+//     } 
